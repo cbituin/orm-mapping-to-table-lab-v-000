e34010e93e9bafc08d728bfa5a5079283655a76d
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-22 21:02:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,29 @@
 class Student
 
   attr_accessor :name, :grade
+  attr_reader :id
 
+  def initialize(id, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+
+  end
+
+  def self.drop_table
+
+  end
+
+  def save
+
+  end
+
+  def self.create
+
+  end
 
 
 end
